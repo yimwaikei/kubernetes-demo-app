2378d14921ad9dbec6634b5934964909c7f2f67b
Wai-Kei Yim <yimwaikei@gmail.com>
2026-03-24 11:26:39 +0800

    Commit frontend code to test github action

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,7 +1,7 @@
 function About() {
   return <div>
     <h1>About</h1>
-    <p>This web app allows user to upload image, which will transform it to grayscale.</p>
+    <p>This web app allows user to upload image, which will transform it to grayscale. Users can download the grayscale images.</p>
   </div>;
 }
 
